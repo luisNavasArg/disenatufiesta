a10f63a5a4c9a0ea73b116750355dcbe7d77b066
luisNavasArg <lpipnavas@gmail.com>
2017-05-28 22:59:14 -0300

    bueno que vaina quiero guardar los cambios

--- js/Java.js
+++ js/Java.js
@@ -1,30 +1,106 @@
-document.addEventListener('DOMContentLoaded', aplicar);
-	function aplicar(){
-		//var DecoraNavidad= document.getElementById("DecoraNavidad");
-		//var DecoraCumple= document.getElementById("DecoraCumple");
-		function mostrar(){
+document.addEventListener('DOMContentLoaded', principal);
+	function principal(){
 
-   			var elemento = document.getElementById("formulario");
-
-			elemento.style.visibility= "visible";
-		}
-		function Cerrar(){
-			var cerrar= document.getElementById("formulario");
-   			cerrar.style.visibility="hidden";
-		}
-		function mostrarsubmenu(identificador, estilo){
-   			if(identificador=="DecoraBanderines"){alert('hola');
-	   			var cerrar= document.getElementById(identificador);
-	   			cerrar.style.visibility=estilo;
-	   			DecoraNavidad.style.visibility="hidden";
-	   			DecoraCumple.style.visibility="hidden";
-   			}
-   		}
-		function ocultarsubmenu(){
-   			var submenu = document.getElementById("DecoraPapel");
-   			submenu.style.visibility="hidden";}
-		function lili(){
-   			/*document.addEventListener("click",cerrar);*/
-   			//document.addEventListener("click", capturatop);
+	}
+	function mostrar(){
+		var elemento = document.getElementById("formulario");
+	  	elemento.style.visibility= "visible";
+	}
+	function Cerrar(){
+		var cerrar= document.getElementById("formulario");
+	   	cerrar.style.visibility="hidden";
+	}
+	
+	function muestra(ide,ide2){
+		var anade = document.getElementById(ide);
+		anade.classList.add('muestra');
+		var anade2 = document.getElementById(ide2);
+		anade2.classList.add('muestra');
+	}
+	function ocultarVarios(ident){
+		if(ident=='cp'){
+			oculta('db1');
+			oculta('db2');
+			oculta('nv1');
+			oculta('nv2');
+			oculta('cl1');
+			oculta('cl2');
+			oculta('vi1');
+			oculta('vi2');
+			oculta('mt1');
+			oculta('mt2');
+		} else if(ident=='db'){
+			oculta('cp1');
+			oculta('cp2');
+			oculta('nv1');
+			oculta('nv2');
+			oculta('cl1');
+			oculta('cl2');
+			oculta('vi1');
+			oculta('vi2');
+			oculta('mt1');
+			oculta('mt2');
+		} else if(ident=='nv'){
+			oculta('db1');
+			oculta('db2');
+			oculta('cp1');
+			oculta('cp2');
+			oculta('cl1');
+			oculta('cl2');
+			oculta('vi1');
+			oculta('vi2');
+			oculta('mt1');
+			oculta('mt2');
+		} else if (ident=='cl'){
+			oculta('db1');
+			oculta('db2');
+			oculta('cp1');
+			oculta('cp2');
+			oculta('nv1');
+			oculta('nv2');
+			oculta('vi1');
+			oculta('vi2');
+			oculta('mt1');
+			oculta('mt2');
+		}else if (ident=='vi'){
+			oculta('db1');
+			oculta('db2');
+			oculta('cp1');
+			oculta('cp2');
+			oculta('nv1');
+			oculta('nv2');
+			oculta('cl1');
+			oculta('cl2');
+			oculta('mt1');
+			oculta('mt2');
+		}else if (ident=='mt'){
+			oculta('db1');
+			oculta('db2');
+			oculta('cp1');
+			oculta('cp2');
+			oculta('nv1');
+			oculta('nv2');
+			oculta('cl1');
+			oculta('cl2');
+			oculta('vi1');
+			oculta('vi2');
+		}else if (ident=='ct'){
+			oculta('db1');
+			oculta('db2');
+			oculta('cp1');
+			oculta('cp2');
+			oculta('nv1');
+			oculta('nv2');
+			oculta('cl1');
+			oculta('cl2');
+			oculta('vi1');
+			oculta('vi2');
+			oculta('mt1');
+			oculta('mt2');
 		}
 	}
+	function oculta(id){
+		var ocul = document.getElementById(id);
+		ocul.classList.remove('muestra');
+	}
+
