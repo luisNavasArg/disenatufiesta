96a8c2bd48101f63d6620b8f4c6dedeef6dd8ce1
luisNavasArg <lpipnavas@gmail.com>
2017-05-31 19:01:58 -0300

    Update logeando.php
    comentarios de el proceso

--- paginas/logeando.php
+++ paginas/logeando.php
@@ -2,15 +2,17 @@
 session_start();
 
 //var_dump($_POST);
-//$_SESSION["username"]="carlos";
+//$_SESSION["username"]="luis";
 //$_SESSION["password"]="123456";
 
-
+//con isset podemos validar que estan accediendo por medio del formulario
 if(isset($_POST['submit'])){
-$username="carlos";
+//acceder a nestra base de datos para verificar los usuarios y su clave
+$username="luis";
 $password="123456";
+//validamos que no esten vacio el usuario y la clave
 if((isset($_POST['nombre']))&&(isset($_POST['password']))){
-
+//capturamos en una variable el usuario y clave que llegan del formulario
 $nombre=$_POST['nombre'];
 $pass=$_POST['password'];
 echo('existen '.$nombre.' y '.$pass);
